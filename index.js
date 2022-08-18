0372fd27663713ded3efd8cf8577607eb7edf892
Noahkimtai <noahkimtai24@gmail.com>
2022-08-18 11:26:07 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+//function multiply()
+let num1 = 1
+let num2 = 62
+let multiply = num1*num2
+let random  = Number.parseInt(Math.random()*10)
+let num3=5, num4 =  19;
+let mod = num4%num3
+let max = Math.max(1,2,3,20)
+console.log(max)
